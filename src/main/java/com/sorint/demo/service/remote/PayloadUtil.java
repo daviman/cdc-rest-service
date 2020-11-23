7aecfb389f91ad9c3f4088f91e8b3c684b52837a
daviman <daviman@gmail.com>
2020-11-23 13:02:16 -0800

    made serializable

--- src/main/java/com/sorint/demo/service/remote/PayloadUtil.java
+++ src/main/java/com/sorint/demo/service/remote/PayloadUtil.java
@@ -3,12 +3,13 @@ package com.sorint.demo.service.remote;
 import com.sorint.demo.service.data.avro.Article;
 import com.sorint.demo.service.model.PagePayload;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-public class PayloadUtil {
+public class PayloadUtil implements Serializable {
 
     private static void setPageCount(PagePayload pagePayload) {
         BigDecimal number = new BigDecimal(pagePayload.getTotal());
